01a4b29446c4a685e5fc75172cb00486ef980f19
llpuchaicela <73542300+llpuchaicela@users.noreply.github.com>
2020-12-06 13:23:21 -0500

    Programa para imprimir  de 320 a 160

---
+++ Numeros 320  a 160.py
@@ -0,0 +1,10 @@
+print("Ejercicio 23")
+print("Lilibeth Puchaicela")
+print("Programa para imprimir numeros disminuendo en 20")
+
+
+i = 320
+
+while i >= 160:
+	print ("Los numeros son: " ,i)
+	i -= 20
